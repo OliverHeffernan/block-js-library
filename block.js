a8f65a5785a6eb6d892cdfdcf43c280ef6240b45
OliverHeffernan <oliverheffernan@icloud.com>
2024-04-28 17:03:06 +1200

    changes

--- block.js
+++ block.js
@@ -20,7 +20,7 @@ function processImports() {
     if (blocksTag.length > 1)
         return error("Only one <blocks> tag is allowed. Please ensure that all blocks you want to import are listed within a single <blocks> tag, separated by commas.");
     
-    if (blocksTag.length == 0) 
+    if (blocksTag.length == 0)
         return error("Error: The <blocks> tag is missing. Please ensure you have a <blocks> tag in order to use imports.");
 
     // Get the content of the <imports> tag and hide it
